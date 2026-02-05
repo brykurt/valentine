ba3e0d9dbf96d8b7117f818dda6a58611c535a9b
Bryon Kurt Segunto <bryonkurts@gmail.com>
2026-02-05 13:16:16 +0800

    Build GitHub Pages from root

--- scripts/postbuild-gh-pages.js
+++ scripts/postbuild-gh-pages.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const docsDir = path.join(__dirname, '..', 'docs');
-const indexPath = path.join(docsDir, 'index.html');
-const notFoundPath = path.join(docsDir, '404.html');
-const noJekyllPath = path.join(docsDir, '.nojekyll');
+const outputArg = process.argv[2];
+const outputDir = outputArg
+  ? path.resolve(process.cwd(), outputArg)
+  : path.join(__dirname, '..', 'dist', 'will-you-be-my-valentine');
+const indexPath = path.join(outputDir, 'index.html');
+const notFoundPath = path.join(outputDir, '404.html');
+const noJekyllPath = path.join(outputDir, '.nojekyll');
 
 if (!fs.existsSync(indexPath)) {
   console.error('index.html not found. Run the GitHub Pages build first.');
@@ -14,4 +17,4 @@ if (!fs.existsSync(indexPath)) {
 fs.copyFileSync(indexPath, notFoundPath);
 fs.writeFileSync(noJekyllPath, '');
 
-console.log('GitHub Pages artifacts ready: 404.html and .nojekyll');
+console.log(`GitHub Pages artifacts ready in ${outputDir}`);
