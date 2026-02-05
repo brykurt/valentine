993c96f45f45783206aba41e0e3cb409c51e1157
Bryon Kurt Segunto <bryonkurts@gmail.com>
2026-02-05 13:17:43 +0800

    Tweak message and formatting

--- scripts/postbuild-gh-pages.js
+++ scripts/postbuild-gh-pages.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const outputArg = process.argv[2];
 const outputDir = outputArg
   ? path.resolve(process.cwd(), outputArg)
-  : path.join(__dirname, '..', 'dist', 'will-you-be-my-valentine');
-const indexPath = path.join(outputDir, 'index.html');
-const notFoundPath = path.join(outputDir, '404.html');
-const noJekyllPath = path.join(outputDir, '.nojekyll');
+  : path.join(__dirname, "..", "dist", "will-you-be-my-valentine");
+const indexPath = path.join(outputDir, "index.html");
+const notFoundPath = path.join(outputDir, "404.html");
+const noJekyllPath = path.join(outputDir, ".nojekyll");
 
 if (!fs.existsSync(indexPath)) {
-  console.error('index.html not found. Run the GitHub Pages build first.');
+  console.error("index.html not found. Run the GitHub Pages build first.");
   process.exit(1);
 }
 
 fs.copyFileSync(indexPath, notFoundPath);
-fs.writeFileSync(noJekyllPath, '');
+fs.writeFileSync(noJekyllPath, "");
 
 console.log(`GitHub Pages artifacts ready in ${outputDir}`);
